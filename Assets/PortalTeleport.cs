3997b8474e632aaa65574ae37a7a75ef230377b2
sakuszki <madidrzewo69@gmail.com>
2026-05-12 09:42:17 +0200

    Teleport

---
+++ Assets/PortalTeleport.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PortalTeleport :MonoBehaviour
+{
+    public Vector3 destination;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.position = 
+    }
+}
+{
+    
+}
---
+++ Assets/Teleport.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PortalTeleport : MonoBehaviour
+{
+    public Transform destination;
+    //public Vector3 destination;
+    private void OnTriggerEnter2D(Collider2D collison)
+    {
+        collision.transform.position = destination.position;
+        //collision.transform.position = destination;
+
+    }
+    //private void OnDrawGizmosSelected()
+    //{
+    // Gizmos.color = Color.red;
+    // Gizmos.DrawSphere(destination, 1);
+    //}
+}
